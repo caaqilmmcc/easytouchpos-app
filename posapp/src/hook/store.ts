24940f5ec1339a434b8d40a521af4675629a5d41
Mohamed Mohamoud <mohamed.mahmoud@yooltech.com>
2024-05-05 10:12:47 +0300

    this is state management called zustand

--- posapp/src/hook/store.ts
+++ posapp/src/hook/store.ts
@@ -1,35 +1,54 @@
-import { Product } from "@/components/product.vue";
-import create from "vue-zustand";
-import { persist } from "zustand/middleware";
+import { Product } from '@/components/product.vue'
+import create from 'vue-zustand'
+import { persist } from 'zustand/middleware'
 interface CategoryState {
-  isSelectedCategory: string;
-  setSelectedCategory: (category?: string) => void;
+  isSelectedCategory: string
+  setSelectedCategory: (category?: string) => void
 }
 interface ModalState {
-  isOpen: boolean;
-  modalType: string;
-  onOpen: (modalType: string) => void;
-  onClose: () => void;
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+}
+interface ModalSheetState {
+  isOpen: boolean
+  modalType: string
+  onOpen: (name: string) => void
+  onClose: () => void
 }
 interface CartState {
-  cart: Product[];
-  addToCart: (product: Product) => void;
-  removeFromCart: (product: Product) => void;
-  removeAllById: (product: Product) => void;
+  cart: Product[]
+  addToCart: (product: Product) => void
+  removeFromCart: (product: Product) => void
+  removeAllById: (product: Product) => void
 }
+interface SelectPosProfileProps {
+  selectPosProfile: string
+  onSelectedPosProfile: (name: string) => void
+}
+export const useSelectPosProfile = create<SelectPosProfileProps>((set) => ({
+  selectPosProfile: 'akram',
+  onSelectedPosProfile: (name) => set({ selectPosProfile: name }),
+}))
 
 export const useCategory = create<CategoryState>((set) => ({
-  isSelectedCategory: "ALL",
-  setSelectedCategory: (category = "ALL") =>
+  isSelectedCategory: 'ALL',
+  setSelectedCategory: (category = 'ALL') =>
     set({ isSelectedCategory: category }),
-}));
+}))
 
 export const useModal = create<ModalState>((set) => ({
+  isOpen: true,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}))
+
+export const useModalSheet = create<ModalSheetState>((set) => ({
   isOpen: false,
-  modalType: "",
-  onOpen: (modalType) => set({ isOpen: true, modalType }),
-  onClose: () => set({ isOpen: false, modalType: "" }),
-}));
+  modalType: '',
+  onOpen: (name) => set({ isOpen: true, modalType:name }),
+  onClose: () => set({ isOpen: false, modalType: '' }),
+}))
 
 export const useCartStore = create<CartState>()(
   persist(
@@ -38,27 +57,25 @@ export const useCartStore = create<CartState>()(
       addToCart: (product: Product) => {
         set((state) => ({
           cart: [...state.cart, product],
-        }));
+        }))
       },
       removeFromCart: (product: Product) => {
-        const productToRemove = get().cart.findIndex(
-          (p) => p.id === product.id
-        );
+        const productToRemove = get().cart.findIndex((p) => p.id === product.id)
         set((state) => {
-          const newCart = [...state.cart];
-          newCart.splice(productToRemove, 1);
-          return { cart: newCart };
-        });
+          const newCart = [...state.cart]
+          newCart.splice(productToRemove, 1)
+          return { cart: newCart }
+        })
       },
       removeAllById: (product: Product) => {
         set((state) => {
-          const newCart = state.cart.filter((c) => c.id != product.id);
-          return { cart: newCart };
-        });
+          const newCart = state.cart.filter((c) => c.id != product.id)
+          return { cart: newCart }
+        })
       },
     }),
     {
-      name: "Shopping cart store",
+      name: 'Shopping cart store',
     }
   )
-);
+)
