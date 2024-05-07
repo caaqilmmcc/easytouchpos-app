620d5d78c5e0363c51d3d710af0dc6c37b515136
Mohamed Mohamoud <mohamed.mahmoud@yooltech.com>
2024-05-07 09:54:51 +0300

    this is build close shit

---
+++ posawesome/public/posapp/assets/recent-sales.0c861084.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,f as c}from"./vendor.91551698.js";const n={};function o(t,a){return r(),c("h1",null," hello ")}var l=e(n,[["render",o]]);export{l as default};
--- posawesome/public/posapp/assets/recent-sales.24ccee0a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,f as c}from"./vendor.10b8f296.js";const n={};function o(t,a){return r(),c("h1",null," hello ")}var l=e(n,[["render",o]]);export{l as default};
