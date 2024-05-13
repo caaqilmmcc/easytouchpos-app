82ef67846f076e61eb2cae37da2a1357c15db2df
Mohamed Mohamoud <mohamed.mahmoud@yooltech.com>
2024-05-13 13:05:31 +0300

    change header and subcategory and make build frappe

--- posapp/src/components/header.vue
+++ posapp/src/components/header.vue
@@ -26,9 +26,9 @@ userProfiel()
 </script>
 <template>
   <header
-    class="flex h-[57px] items-center justify-between gap-1 border-b bg-background px-4"
+    class="flex h-[57px] items-center justify-between gap-4 border-b bg-background px-4"
   >
-  <div class="relative w-full max-w-sm items-center">
+  <div class="relative w-full  items-center">
     <Input id="search" type="text" placeholder="Search..." class="pl-10" />
     <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
       <Search class="size-6 text-muted-foreground" />
